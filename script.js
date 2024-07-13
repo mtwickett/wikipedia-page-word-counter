7b018c4043d7f6e82a4308c2a810fa0463ecc659
MARTIN WICKETT <martintwickett@gmail.com>
2024-07-13 15:46:56 -0400

    first working code

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-import { isWikipediaUrl, getWikipediaTitle } from './utils';
+import { isWikipediaUrl, getWikipediaTitle, getParagraphTags, countWords } from './utils.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('word-count-form').addEventListener('submit', (event) => {
         event.preventDefault()
         const url = document.getElementById('wikipedia-url').value;
-        console.log(url)
 
         if (!isWikipediaUrl(url)) {
             alert('Please enter a valid Wikipedia URL.');
@@ -13,7 +12,19 @@ document.addEventListener('DOMContentLoaded', () => {
         };
 
         const title = getWikipediaTitle(url);
-        console.log(title)
+        
+        fetch(`https://en.wikipedia.org/w/api.php?action=parse&page=${title}&prop=text&formatversion=2&origin=*&format=json`)
+            .then(response => response.json())
+            .then(data => {
+                if (!data.parse) {
+                    throw new Error('API response does not contain parse data');
+                }
+                const text = getParagraphTags(data.parse.text);
+                const wordCount = countWords(text);
+                document.getElementById('number-of-words').textContent = wordCount;
+                document.getElementById('word-count-display').classList.remove('hidden');
+            })
+            .catch(error => console.error('Error:', error));
     });
 });
 
